f065a690285343ea8e565e9ce109e0bcbda811ee
KaitoMiyuki <proappdeveloper@protonmail.com>
2024-10-20 21:13:40 +0200

    update translation keys

--- src/components/CodeOfConduct/codeOfConductItems.ts
+++ src/components/CodeOfConduct/codeOfConductItems.ts
@@ -1,15 +1,15 @@
 import { AccordionItemContent } from "../Accordion/AccordionProps";
 
 export const codeOfConductItems: AccordionItemContent[] = [
-  { heading: "accordionLoveRespectHeading", content: "accordionLoveRespectContent" },
-  { heading: "accordionOpenCommunicationHeading", content: "accordionOpenCommunicationContent" },
-  { heading: "accordionSupportUnityHeading", content: "accordionSupportUnityContent" },
-  { heading: "accordionTimeTogetherHeading", content: "accordionTimeTogetherContent" },
-  { heading: "accordionResponsibilityHeading", content: "accordionResponsibilityContent" },
-  { heading: "accordionDiversityAcceptanceHeading", content: "accordionDiversityAcceptanceContent" },
-  { heading: "accordionEducationDevelopmentHeading", content: "accordionEducationDevelopmentContent" },
-  { heading: "accordionGratitudeHeading", content: "accordionGratitudeContent" },
-  { heading: "accordionConflictResolutionHeading", content: "accordionConflictResolutionContent" },
-  { heading: "accordionSafetySecurityHeading", content: "accordionSafetySecurityContent" },
-  { heading: "accordionCommunityServiceHeading", content: "accordionCommunityServiceContent" }
+  { heading: "codeOfConductLoveRespectHeading", content: "codeOfConductLoveRespectContent" },
+  { heading: "codeOfConductOpenCommunicationHeading", content: "codeOfConductOpenCommunicationContent" },
+  { heading: "codeOfConductSupportUnityHeading", content: "codeOfConductSupportUnityContent" },
+  { heading: "codeOfConductTimeTogetherHeading", content: "codeOfConductTimeTogetherContent" },
+  { heading: "codeOfConductResponsibilityHeading", content: "codeOfConductResponsibilityContent" },
+  { heading: "codeOfConductDiversityAcceptanceHeading", content: "codeOfConductDiversityAcceptanceContent" },
+  { heading: "codeOfConductEducationDevelopmentHeading", content: "codeOfConductEducationDevelopmentContent" },
+  { heading: "codeOfConductGratitudeHeading", content: "codeOfConductGratitudeContent" },
+  { heading: "codeOfConductConflictResolutionHeading", content: "codeOfConductConflictResolutionContent" },
+  { heading: "codeOfConductSafetySecurityHeading", content: "codeOfConductSafetySecurityContent" },
+  { heading: "codeOfConductCommunityServiceHeading", content: "codeOfConductCommunityServiceContent" }
 ];
